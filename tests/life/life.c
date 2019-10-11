6e72e4c3c2da69c5f1da63cc84090cedcaf277c2
James Tingen <jotingen@gmail.com>
2019-10-11 00:26:54 -0400

    Added a framebuffer for the graphic display

--- tests/life/life.c
+++ tests/life/life.c
@@ -30,19 +30,24 @@ uint32_t xorshift(uint32_t lfsr) {
 
 void paint_framebuffer(int rows, int cols, uint8_t life[rows][cols]) {
   display_pixel_t pixel;
-  display_write_start();
   for(int row = display_rows()-1; row >= 0; row--) {
     for(int col = display_cols()-1; col >= 0; col--) {
       if(life[row][col] == 0) {
-        pixel.R = 0x00;
+        pixel.R = 0xFF;
         pixel.G = 0xFF;
         pixel.B = 0xFF;
       } else {
-        pixel.R = 0xFF;
+        pixel.R = 0x00;
         pixel.G = 0x00;
         pixel.B = 0x00;
       }
-      display_write_pixel(pixel);
+      display_write_buffer(row,col,pixel);
+    }
+  }
+  display_write_start();
+  for(int row = display_rows()-1; row >= 0; row--) {
+    for(int col = display_cols()-1; col >= 0; col--) {
+      display_write_pixel(display_read_buffer(row,col));
     }
   }
 }
@@ -55,9 +60,9 @@ void main(void) {
 
   uint32_t key_pressed;
 
-  uint64_t timestamp;
-  uint64_t keystamp;
-  uint64_t displaystamp;
+  //uint64_t timestamp;
+  //uint64_t keystamp;
+  //uint64_t displaystamp;
 
   uint32_t led;
 
@@ -66,85 +71,99 @@ void main(void) {
   uint8_t life[display_rows()][display_cols()];
   uint8_t life_next[display_rows()][display_cols()];
 
+  display_pixel_t test_pixel;
+
   lfsr = 0xAAAAAAAA;
 
       LED = 0;
+        test_pixel.R = 0x00;
+        test_pixel.G = 0x3F;
+        test_pixel.B = 0x3F;
+      LED = 2;
+      display_write_buffer(0,0,test_pixel);
+        test_pixel.R = 0x3F;
+        test_pixel.G = 0x00;
+        test_pixel.B = 0x00;
+      display_write_buffer(display_rows()-1,display_cols()-1,test_pixel);
+      LED = 3;
+      test_pixel = display_read_buffer(0,0);
+      LED = 4;
+      test_pixel = display_read_buffer(display_rows()-1,display_cols()-1);
+      LED = 5;
 
   display_on();
 
-      LED = 1;
+      LED = 6;
 
   //Get initial timestamps
-  timestamp   = get_time();
-  keystamp    = timestamp;
+  //timestamp   = get_time();
+  //keystamp    = timestamp;
 
-      LED = 2;
-      LED = lfsr;
+      LED = 7;
+      //LED = lfsr;
   //Initialize life
   for(int row = 0; row < display_rows(); row++) {
     for(int col = 0; col < display_cols(); col++) {
       lfsr = xorshift(lfsr);
-      LED = lfsr;
       if(lfsr%3 == 0) { 
-      //if((row & 0x1) & (col & 0x1)) { 
         life[row][col] = 1;
       } else {
         life[row][col] = 0;
       }
     }
   }
-      LED = 3;
+      LED = 8;
 
   //Main loop
   while(1) {
     //Get new timestamp
-    timestamp = get_time();
+    //timestamp = get_time();
 
     //Get keypresses if the delta between the current timestamp and last keystamp is >= KEY_TIME
-    if ((timestamp-keystamp) >= KEY_TIME) {
-      if(BUTTON_0        == 1) {key_pressed = 1; but0      = 1;} 
-      if(BUTTON_1        == 1) {key_pressed = 1; but1      = 1;} 
-      if(JOYSTICK_SELECT == 1) {key_pressed = 1; joy_sel   = 1;}    
-      if(JOYSTICK_UP     == 1) {key_pressed = 1; joy_up    = 1;} 
-      if(JOYSTICK_DOWN   == 1) {key_pressed = 1; joy_down  = 1;} 
-      if(JOYSTICK_LEFT   == 1) {key_pressed = 1; joy_left  = 1;} 
-      if(JOYSTICK_RIGHT  == 1) {key_pressed = 1; joy_right = 1;} 
-      keystamp = timestamp;
-    }
+    //if ((timestamp-keystamp) >= KEY_TIME) {
+    //  if(BUTTON_0        == 1) {key_pressed = 1; but0      = 1;} 
+    //  if(BUTTON_1        == 1) {key_pressed = 1; but1      = 1;} 
+    //  if(JOYSTICK_SELECT == 1) {key_pressed = 1; joy_sel   = 1;}    
+    //  if(JOYSTICK_UP     == 1) {key_pressed = 1; joy_up    = 1;} 
+    //  if(JOYSTICK_DOWN   == 1) {key_pressed = 1; joy_down  = 1;} 
+    //  if(JOYSTICK_LEFT   == 1) {key_pressed = 1; joy_left  = 1;} 
+    //  if(JOYSTICK_RIGHT  == 1) {key_pressed = 1; joy_right = 1;} 
+    //  keystamp = timestamp;
+    //}
 
                                      
     //Update LED/Display if the delta between the current timestamp and last displaystamp is >= DISPLAY_TIME  
     //Reset timer
     //Reset buttons
-    if(timestamp >= DISPLAY_TIME) {
-      //Prep LED
-      led = 0;
+    //if(timestamp >= DISPLAY_TIME) {
+      ////Prep LED
+      //led = 0;
 
-      //If key_pressed, change LED to reflect keypress
-      if(key_pressed) {
-        if(but0     ) led = led | 0x01;
-        if(but1     ) led = led | 0x02;
-        if(joy_sel  ) led = led | 0x04;
-        if(joy_up   ) led = led | 0x08;
-        if(joy_down ) led = led | 0x10;
-        if(joy_left ) led = led | 0x20;
-        if(joy_right) led = led | 0x40;
-      }
+      ////If key_pressed, change LED to reflect keypress
+      //if(key_pressed) {
+      //  if(but0     ) led = led | 0x01;
+      //  if(but1     ) led = led | 0x02;
+      //  if(joy_sel  ) led = led | 0x04;
+      //  if(joy_up   ) led = led | 0x08;
+      //  if(joy_down ) led = led | 0x10;
+      //  if(joy_left ) led = led | 0x20;
+      //  if(joy_right) led = led | 0x40;
+      //}
 
-      //Update LED
-      LED = led;
+      ////Update LED
+      //LED = led;
 
       //Update frame buffer
-      LED = 4;
+      LED = 9;
       paint_framebuffer(display_rows(),display_cols(),life);
         
-      LED = 5;
+      LED = 10;
       //Reset timers
-      clear_time();
-      timestamp = 0;
-      keystamp  = 0;
+      //clear_time();
+      //timestamp = 0;
+      //keystamp  = 0;
 
-      LED = 6;
+      LED = 11;
       //if(but0     ) {
         //Update life
         for(int row = 0; row < display_rows(); row++) {
@@ -207,7 +226,7 @@ void main(void) {
             }
           }
         }
-        LED = 7;
+        LED = 12;
         for(int row = 0; row < display_rows(); row++) {
           for(int col = 0; col < display_cols(); col++) {
               life[row][col] = life_next[row][col];
@@ -216,7 +235,7 @@ void main(void) {
       //while(BUTTON_0        == 1) {} 
       //}
 
-      LED = 8;
+      LED = 13;
 
       //Clear keypresses
       but0        = 0;
@@ -228,7 +247,7 @@ void main(void) {
       joy_right   = 0;
       key_pressed = 0;
 
-    }
+    //}
 
   }
 }
