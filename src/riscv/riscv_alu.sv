c20ff05e091d7c6ab87ca793be3cc665bc3b90b2
James Tingen <jotingen@gmail.com>
2019-10-14 22:29:29 -0400

    Fix SRAI bug

--- src/riscv/riscv_alu.sv
+++ src/riscv/riscv_alu.sv
@@ -816,7 +816,7 @@ always_ff @(posedge clk)
                  //$display("%-5s PC=%08X rs1=(%d)%08X imm=%08X rd=(%d)", "SRAI", PC, alu_rs1, rs1_data, alu_imm[4:0], alu_rd);
                  alu_retired <= '1;
                  x_wr[alu_rd] <= '1;
-                 rd_data <= rs1_data >>> alu_shamt;
+                 rd_data <= $signed(rs1_data) >>> alu_shamt;
                  alu_PC <= alu_PC+'d4;
                  end
 
