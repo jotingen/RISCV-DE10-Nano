52764515c48b6f9230a4c247e5d87074f0e6d222
James Tingen <jotingen@gmail.com>
2019-10-31 23:02:23 -0400

    Fix CSRs

--- src/riscv/riscv_csr.sv
+++ src/riscv/riscv_csr.sv
@@ -18,17 +18,49 @@ logic [31:0] csr_C00;
 logic [31:0] csr_C80;
 
 logic [31:0] time_counter;
-logic [31:0] csr_C04;
-logic [31:0] csr_C84;
+logic [31:0] csr_C01;
+logic [31:0] csr_C81;
 
-logic [31:0] csr_C08;
-logic [31:0] csr_C88;
+logic [31:0] csr_C02;
+logic [31:0] csr_C82;
 
 
 always_ff @(posedge clk)
   begin
   csr_ack  <= '0;
   csr_data_rd <= '0;
+
+  //Counter
+  {csr_C80,csr_C00} <= {csr_C80,csr_C00}+'d1;
+
+  //Time
+  if(time_counter >= 'd50000) //Tied to 50 MHz 
+    begin 
+    {csr_C81,csr_C01} <= {csr_C81,csr_C01}+'d1;
+    end 
+  else
+    begin
+    {csr_C81,csr_C01} <= {csr_C81,csr_C01};
+    end 
+  if(time_counter >= 50000) 
+    begin
+    time_counter <= '0;
+    end
+  else
+    begin
+    time_counter <= time_counter + 'd1;
+    end
+
+  //Instructions Retired
+  if (alu_vld & alu_retired)
+    begin 
+    {csr_C82,csr_C02} <= {csr_C82,csr_C02}+'d1;
+    end 
+  else
+    begin
+    {csr_C82,csr_C02} <= {csr_C82,csr_C02};
+    end 
+
 
   if(csr_req & ~csr_write)
     begin
@@ -38,16 +70,25 @@ always_ff @(posedge clk)
       'hC00: begin
              csr_data_rd <= csr_C00;
              end
+      'hC01: begin
+             csr_data_rd <= csr_C01;
+             end
+      'hC02: begin
+             csr_data_rd <= csr_C02;
+             end
       'hC80: begin
              csr_data_rd <= csr_C80;
+             end
+      'hC81: begin
+             csr_data_rd <= csr_C81;
+             end
+      'hC82: begin
+             csr_data_rd <= csr_C82;
              end
       default: begin
                end
     endcase
     end
-
-  //Counter
-  {csr_C80,csr_C00} <= {csr_C80,csr_C00}+'d1;
 
   if(csr_req & csr_write)
     begin
@@ -58,75 +99,24 @@ always_ff @(posedge clk)
              csr_C00 <= (csr_C00     & ~csr_mask) | 
                         (csr_data_wr & ~csr_mask);
              end
+      'hC01: begin
+             csr_C01 <= (csr_C01     & ~csr_mask) | 
+                        (csr_data_wr & ~csr_mask);
+             end
+      'hC02: begin
+             csr_C02 <= (csr_C02     & ~csr_mask) | 
+                        (csr_data_wr & ~csr_mask);
+             end
       'hC80: begin
              csr_C80 <= (csr_C80     & ~csr_mask) | 
                         (csr_data_wr & ~csr_mask);
              end
-      default: begin
-               end
-    endcase
-    end
-
-
-  //Time
-  if(time_counter >= 'd50000) //Tied to 50 MHz 
-    begin 
-    {csr_C84,csr_C04} <= {csr_C84,csr_C04}+'d1;
-    end 
-  else
-    begin
-    {csr_C84,csr_C04} <= {csr_C84,csr_C04};
-    end 
-
-  if(csr_req & csr_write)
-    begin
-    csr_ack <= '1;
-    unique
-    case (csr_addr)
-      'hC04: begin
-             csr_C04 <= (csr_C04     & ~csr_mask) | 
+      'hC81: begin
+             csr_C81 <= (csr_C81     & ~csr_mask) | 
                         (csr_data_wr & ~csr_mask);
              end
-      'hC84: begin
-             csr_C84 <= (csr_C84     & ~csr_mask) | 
-                        (csr_data_wr & ~csr_mask);
-             end
-      default: begin
-               end
-    endcase
-    end
-
-  if(time_counter >= 50000) 
-    begin
-    time_counter <= '0;
-    end
-  else
-    begin
-    time_counter <= time_counter + 'd1;
-    end
-
-
-  //Instructions Retired
-  if (alu_vld & alu_retired)
-    begin 
-    {csr_C88,csr_C08} <= {csr_C88,csr_C08}+'d1;
-    end 
-  else
-    begin
-    {csr_C88,csr_C08} <= {csr_C88,csr_C08};
-    end 
-
-  if(csr_req & csr_write)
-    begin
-    csr_ack <= '1;
-    unique
-    case (csr_addr)
-      'hC08: begin
-             csr_C08 <= (csr_C08     & ~csr_mask) | 
-                        (csr_data_wr & ~csr_mask);
-             end
-      'hC88: begin
-             csr_C88 <= (csr_C88     & ~csr_mask) | 
+      'hC82: begin
+             csr_C82 <= (csr_C82     & ~csr_mask) | 
                         (csr_data_wr & ~csr_mask);
              end
       default: begin
@@ -141,11 +131,11 @@ always_ff @(posedge clk)
     csr_C80  <= '0;
 
     time_counter <= '0;
-    csr_C04  <= '0;
-    csr_C84  <= '0;
+    csr_C01  <= '0;
+    csr_C81  <= '0;
 
-    csr_C08  <= '0;
-    csr_C88  <= '0;
+    csr_C02  <= '0;
+    csr_C82  <= '0;
     end
   end
 
