0731b126e8e5e0655abffecdddc6c702c0e802ed
James Tingen <jotingen@gmail.com>
2020-03-10 23:26:26 -0400

    Update testbench to print out UART. Disable rvfi monitor since branch mispredicts not handled correctly.

--- programs/benchmarks/primes.c
+++ programs/benchmarks/primes.c
@@ -2,28 +2,68 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include "../lib/display.h"
+
 #define LED             (*((volatile unsigned int *) (0xC0000000)))
+
+uint32_t sqrt(uint32_t x)  
+{  
+    register uint32_t op, res, one;  
+  
+    op = x;  
+    res = 0;  
+  
+    /* "one" starts at the highest power of four <= than the argument. */  
+    one = 1 << 30;  /* second-to-top bit set */  
+    while (one > op) one >>= 2;  
+  
+    while (one != 0) {  
+        if (op >= res + one) {  
+            op -= res + one;  
+            res += one << 1;  // <-- faster than 2 * one  
+        }  
+        res >>= 1;  
+        one >>= 2;  
+    }  
+    return res;  
+}
 
 int main()
 {
+	
+        //set_uart_baud(921600);
+        char buffer[80];
+  	console_clear();
+        console_puts("Prime calculation benchmark\n");
+        console_puts("---------------------------\n");
+        console_putc('\n');
 	LED = 0;
-	uint64_t limit = 255;
-	size_t primes_size = (limit) * sizeof(uint64_t);
-	uint64_t *primes = (uint64_t*)malloc(primes_size);
-	for(uint64_t n = 2; n < limit; n++) {
-		LED = n;
-		primes[n] = 1;
-		for(uint64_t i = 2; i <= n; ++i) {
-			if(n % i == 0) {
-				primes[n] = 0;
+	uint32_t limit = 255;
+	size_t primes_size = (limit) * sizeof(uint32_t);
+	uint32_t *primes = (uint32_t*)malloc(primes_size);
+	uint32_t prime_count = 1; //Seed 2 as first prime
+	primes[0] = 2;            //Seed 2 as first prime
+        LED = 2;
+        //console_puts(uint8_to_hex(LED));
+	//console_putc('\n');
+        sprintf(buffer,"%d\n",(uint32_t)LED);
+        console_puts(buffer);
+	for(uint32_t n = 3; n < limit; n++) {
+		int is_prime = 1;
+		for(uint32_t p = 0; p < prime_count; ++p) {
+			if(n % primes[p] == 0) {
+				is_prime = 0;
 				break;
 			}
 		}
-	}
-	LED = 0;
-	for(uint64_t n = 2; n < limit; n++) {
-		if(primes[n]) {
+		if(is_prime) {
+			primes[prime_count] = n;
+			prime_count++;
 			LED = n;
+			//console_puts(uint8_to_hex(LED));
+			//console_putc('\n');
+                        sprintf(buffer,"%d\n",(uint32_t)LED);
+                        console_puts(buffer);
 		}
 	}
 	while(1);
--- programs/bootloader/bootloader_preloaded.c
+++ programs/bootloader/bootloader_preloaded.c
@@ -5,9 +5,15 @@
 #include "../lib/rand.h"
 #include "../lib/sdcard.h"
 
+#include "../lib/display.h"
+
 #define LED             (*((volatile unsigned int *) (0xC0000000)))
 
 void main(void) {
+
+  //Set UART crazy fast for sims
+  //set_uart_baud(921600);
+  set_uart_baud(9216000);
 
   //Straight to app, preload in ddr first
   ((void (*)(void))0x10000000)();
