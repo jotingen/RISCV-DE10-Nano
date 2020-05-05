42c199ac3d212066e5be784b447e3ecb0626a69e
James Tingen <jotingen@gmail.com>
2020-05-05 17:06:06 -0400

    Remove old files

--- build.bat
+++
@@ -1,7 +0,0 @@
-SET PATH=%PATH%;"C:\intelFPGA_lite\18.1\quartus\bin64\"
-
-quartus_map --read_settings_files=on --write_settings_files=off DE10Nano -c DE10Nano && ^
-quartus_fit --read_settings_files=off --write_settings_files=off DE10Nano -c DE10Nano  && ^
-quartus_asm --read_settings_files=off --write_settings_files=off DE10Nano -c DE10Nano  && ^
-quartus_sta DE10Nano -c DE10Nano
-
--- wave.bat
+++
@@ -1,5 +0,0 @@
-SET PATH=%PATH%;"C:\Program Files\gtkwave64\bin"
-
-cd simulation\modelsim
-gtkwave.exe .\sim.vcd .\RISCV.gtkw
-
