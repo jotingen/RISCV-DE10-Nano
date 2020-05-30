169edef6a5236ac6e8886af7287d91bf685a70b5
James Tingen <jotingen@gmail.com>
2020-05-30 15:01:04 -0400

    Make DDR3 model latency configurable

--- verif/ddr3/ddr3_model.sv
+++ verif/ddr3/ddr3_model.sv
@@ -11,9 +11,11 @@ module ddr3_model (
   input  logic [8:0]   ddr3_avl_size        
 );
 
+localparam DELAY = 1;
+
 logic [127:0] ddr3 [2**(26-4)-1:0];
-logic         ddr3_avl_rdata_valid_stg1; 
-logic [127:0] ddr3_avl_rdata_stg1;       
+logic [DELAY:0]        ddr3_avl_rdata_valid_stg; 
+logic [DELAY:0][127:0] ddr3_avl_rdata_stg;       
 logic         ddr3_avl_rdata_valid_stg2; 
 logic [127:0] ddr3_avl_rdata_stg2;       
 logic         ddr3_avl_rdata_valid_stg3; 
@@ -26,14 +28,14 @@ logic [127:0] ddr3_avl_rdata_stg5;
 always_ff @(posedge clk)
   begin
   ddr3_avl_ready <= '0;
-  ddr3_avl_rdata_valid_stg1 <= '0; 
+  ddr3_avl_rdata_valid_stg[0] <= '0; 
   ddr3_avl_rdata <= '0;       
 
   if(ddr3_avl_read_req) 
     begin
     ddr3_avl_ready <= '0;
-    ddr3_avl_rdata_valid_stg1 <= '1;
-    ddr3_avl_rdata_stg1 <= ddr3[ddr3_avl_addr[25:4]];
+    ddr3_avl_rdata_valid_stg[0] <= '1;
+    ddr3_avl_rdata_stg[0] <= ddr3[ddr3_avl_addr[25:4]];
     end
   if(ddr3_avl_write_req) 
     begin
@@ -41,20 +43,14 @@ always_ff @(posedge clk)
     ddr3[ddr3_avl_addr[25:4]] <= ddr3_avl_wdata;
     end
 
-  ddr3_avl_rdata_valid_stg2 <= ddr3_avl_rdata_valid_stg1;
-  ddr3_avl_rdata_stg2       <= ddr3_avl_rdata_stg1;     
+  for(int i = 1; i <= DELAY; i++)
+  begin
+    ddr3_avl_rdata_valid_stg[i] <= ddr3_avl_rdata_valid_stg[i-1];
+    ddr3_avl_rdata_stg[i]       <= ddr3_avl_rdata_stg[i-1];     
+  end
   
-  ddr3_avl_rdata_valid_stg3 <= ddr3_avl_rdata_valid_stg2;
-  ddr3_avl_rdata_stg3       <= ddr3_avl_rdata_stg2;     
-  
-  ddr3_avl_rdata_valid_stg4 <= ddr3_avl_rdata_valid_stg3;
-  ddr3_avl_rdata_stg4       <= ddr3_avl_rdata_stg3;     
-  
-  ddr3_avl_rdata_valid_stg5 <= ddr3_avl_rdata_valid_stg4;
-  ddr3_avl_rdata_stg5       <= ddr3_avl_rdata_stg4;     
-  
-  ddr3_avl_rdata_valid      <= ddr3_avl_rdata_valid_stg5;
-  ddr3_avl_rdata            <= ddr3_avl_rdata_stg5;     
+  ddr3_avl_rdata_valid      <= ddr3_avl_rdata_valid_stg[DELAY];
+  ddr3_avl_rdata            <= ddr3_avl_rdata_stg[DELAY];     
   
   end
 
