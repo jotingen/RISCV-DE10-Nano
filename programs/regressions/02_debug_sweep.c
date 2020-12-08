dea5d9081b3e938df01ac815a9f06d0d73be8e3c
James Tingen <jotingen@gmail.com>
2020-12-07 19:22:18 -0500

    Speed up sweep test

--- programs/regressions/02_debug_sweep.c
+++ programs/regressions/02_debug_sweep.c
@@ -8,14 +8,22 @@ void main(void) {
 
   LED_set(0);
 
-  for(uint16_t i = 0; i < DEBUG_ENTRIES_32B; i++) {
+  for(uint16_t i = 0; i < DEBUG_ENTRIES_32B/16; i++) {
+    DEBUG_set32(i, (i<<16)|i);
+  }
+  for(uint16_t i = DEBUG_ENTRIES_32B/16*15; i < DEBUG_ENTRIES_32B; i++) {
     DEBUG_set32(i, (i<<16)|i);
   }
 
   LED_set(0x0A);
   LED_set(0);
 
-  for(uint16_t i = 0; i < DEBUG_ENTRIES_32B; i++) {
+  for(uint16_t i = 0; i < DEBUG_ENTRIES_32B/16; i++) {
+    if(DEBUG_get32(i) != ((i<<16)|i)) {
+      LED_set(0x01);
+    }
+  }
+  for(uint16_t i = DEBUG_ENTRIES_32B/16*15; i < DEBUG_ENTRIES_32B; i++) {
     if(DEBUG_get32(i) != ((i<<16)|i)) {
       LED_set(0x01);
     }
