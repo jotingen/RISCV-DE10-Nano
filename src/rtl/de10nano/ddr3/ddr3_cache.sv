684d562da53ee78bd56076f60e2077c4be82118c
James Tingen <jotingen@gmail.com>
2021-09-20 03:03:01 -0400

    Hack ddr3_cache bus_o to get around verilator fail

--- src/rtl/de10nano/ddr3/ddr3_cache.sv
+++ src/rtl/de10nano/ddr3/ddr3_cache.sv
@@ -17,13 +17,28 @@ module ddr3_cache #(
   input  logic [$bits(wishbone_pkg::dst_rsp_t)-1:0] dst_o_flat
 );
 wishbone_pkg::bus_req_t bus_i;
-wishbone_pkg::bus_rsp_t bus_o;
+//wishbone_pkg::bus_rsp_t bus_o;
+logic             bus_o_Stall;
+logic             bus_o_Ack;
+logic             bus_o_Err;
+logic             bus_o_Rty;
+logic [31:0]      bus_o_Data;
+logic             bus_o_Tga;
+logic             bus_o_Tgd;
+logic  [3:0]      bus_o_Tgc;
 wishbone_pkg::dst_req_t dst_i;
 wishbone_pkg::dst_rsp_t dst_o;
 always_comb
 begin
   bus_i      = bus_i_flat;
-  bus_o_flat = bus_o;
+  bus_o_flat = {bus_o_Stall,
+                bus_o_Ack,
+                bus_o_Err,
+                bus_o_Rty,
+                bus_o_Data,
+                bus_o_Tga,
+                bus_o_Tgd,
+                bus_o_Tgc};
   dst_i_flat = dst_i;
   dst_o      = dst_o_flat;
 end
@@ -78,9 +93,9 @@ wishbone_buff	bus_buff (
                    bus_i} ),
 	.rdreq        ( bus_rd_ack ),
 	.sclr         ( rst ),
-	.wrreq        ( bus_i.Cyc & bus_i.Stb & ~bus_o.Stall ),
+	.wrreq        ( bus_i.Cyc & bus_i.Stb & ~bus_o_Stall ),
 	.almost_empty ( bus_buff_almost_empty ),
-	.almost_full  ( bus_o.Stall ),
+	.almost_full  ( bus_o_Stall ),
 	.empty        ( bus_buff_empty ),
 	.full         (  ),
 	.q            ( {bus_unused_stgd,
@@ -169,13 +184,13 @@ begin
   flushDone <= '0;
 
 
-  bus_o.Ack      <= '0;
-  bus_o.Err      <= '0;
-  bus_o.Rty      <= '0;
-  bus_o.Data     <= '0;
-  bus_o.Tga      <= '0;
-  bus_o.Tgd      <= '0;
-  bus_o.Tgc      <= '0;
+  bus_o_Ack      <= '0;
+  bus_o_Err      <= '0;
+  bus_o_Rty      <= '0;
+  bus_o_Data     <= '0;
+  bus_o_Tga      <= '0;
+  bus_o_Tgd      <= '0;
+  bus_o_Tgc      <= '0;
 
   bus_rd_ack     <= '0;
 
@@ -315,36 +330,36 @@ begin
                             end
                           end
       state_update: begin             
-                      bus_o.Ack  <= '1;
+                      bus_o_Ack  <= '1;
                       case(bus_i_stgd.Adr[3:2])
                         2'b00: begin
-                               bus_o.Data[31:24] <= bus_i_stgd.Sel[3] ? mem_buffer[mem_buffer_lru_entry].Data[0][31:24] : '0;   
-                               bus_o.Data[23:16] <= bus_i_stgd.Sel[2] ? mem_buffer[mem_buffer_lru_entry].Data[0][23:16] : '0;   
-                               bus_o.Data[15:8]  <= bus_i_stgd.Sel[1] ? mem_buffer[mem_buffer_lru_entry].Data[0][15:8]  : '0;   
-                               bus_o.Data[7:0]   <= bus_i_stgd.Sel[0] ? mem_buffer[mem_buffer_lru_entry].Data[0][7:0]   : '0;   
+                               bus_o_Data[31:24] <= bus_i_stgd.Sel[3] ? mem_buffer[mem_buffer_lru_entry].Data[0][31:24] : '0;   
+                               bus_o_Data[23:16] <= bus_i_stgd.Sel[2] ? mem_buffer[mem_buffer_lru_entry].Data[0][23:16] : '0;   
+                               bus_o_Data[15:8]  <= bus_i_stgd.Sel[1] ? mem_buffer[mem_buffer_lru_entry].Data[0][15:8]  : '0;   
+                               bus_o_Data[7:0]   <= bus_i_stgd.Sel[0] ? mem_buffer[mem_buffer_lru_entry].Data[0][7:0]   : '0;   
                                end
                         2'b01: begin
-                               bus_o.Data[31:24] <= bus_i_stgd.Sel[3] ? mem_buffer[mem_buffer_lru_entry].Data[1][31:24] : '0;   
-                               bus_o.Data[23:16] <= bus_i_stgd.Sel[2] ? mem_buffer[mem_buffer_lru_entry].Data[1][23:16] : '0;   
-                               bus_o.Data[15:8]  <= bus_i_stgd.Sel[1] ? mem_buffer[mem_buffer_lru_entry].Data[1][15:8]  : '0;   
-                               bus_o.Data[7:0]   <= bus_i_stgd.Sel[0] ? mem_buffer[mem_buffer_lru_entry].Data[1][7:0]   : '0;   
+                               bus_o_Data[31:24] <= bus_i_stgd.Sel[3] ? mem_buffer[mem_buffer_lru_entry].Data[1][31:24] : '0;   
+                               bus_o_Data[23:16] <= bus_i_stgd.Sel[2] ? mem_buffer[mem_buffer_lru_entry].Data[1][23:16] : '0;   
+                               bus_o_Data[15:8]  <= bus_i_stgd.Sel[1] ? mem_buffer[mem_buffer_lru_entry].Data[1][15:8]  : '0;   
+                               bus_o_Data[7:0]   <= bus_i_stgd.Sel[0] ? mem_buffer[mem_buffer_lru_entry].Data[1][7:0]   : '0;   
                                end
                         2'b10: begin
-                               bus_o.Data[31:24] <= bus_i_stgd.Sel[3] ? mem_buffer[mem_buffer_lru_entry].Data[2][31:24] : '0;   
-                               bus_o.Data[23:16] <= bus_i_stgd.Sel[2] ? mem_buffer[mem_buffer_lru_entry].Data[2][23:16] : '0;   
-                               bus_o.Data[15:8]  <= bus_i_stgd.Sel[1] ? mem_buffer[mem_buffer_lru_entry].Data[2][15:8]  : '0;   
-                               bus_o.Data[7:0]   <= bus_i_stgd.Sel[0] ? mem_buffer[mem_buffer_lru_entry].Data[2][7:0]   : '0;   
+                               bus_o_Data[31:24] <= bus_i_stgd.Sel[3] ? mem_buffer[mem_buffer_lru_entry].Data[2][31:24] : '0;   
+                               bus_o_Data[23:16] <= bus_i_stgd.Sel[2] ? mem_buffer[mem_buffer_lru_entry].Data[2][23:16] : '0;   
+                               bus_o_Data[15:8]  <= bus_i_stgd.Sel[1] ? mem_buffer[mem_buffer_lru_entry].Data[2][15:8]  : '0;   
+                               bus_o_Data[7:0]   <= bus_i_stgd.Sel[0] ? mem_buffer[mem_buffer_lru_entry].Data[2][7:0]   : '0;   
                                end
                         2'b11: begin
-                               bus_o.Data[31:24] <= bus_i_stgd.Sel[3] ? mem_buffer[mem_buffer_lru_entry].Data[3][31:24] : '0;   
-                               bus_o.Data[23:16] <= bus_i_stgd.Sel[2] ? mem_buffer[mem_buffer_lru_entry].Data[3][23:16] : '0;   
-                               bus_o.Data[15:8]  <= bus_i_stgd.Sel[1] ? mem_buffer[mem_buffer_lru_entry].Data[3][15:8]  : '0;   
-                               bus_o.Data[7:0]   <= bus_i_stgd.Sel[0] ? mem_buffer[mem_buffer_lru_entry].Data[3][7:0]   : '0;   
+                               bus_o_Data[31:24] <= bus_i_stgd.Sel[3] ? mem_buffer[mem_buffer_lru_entry].Data[3][31:24] : '0;   
+                               bus_o_Data[23:16] <= bus_i_stgd.Sel[2] ? mem_buffer[mem_buffer_lru_entry].Data[3][23:16] : '0;   
+                               bus_o_Data[15:8]  <= bus_i_stgd.Sel[1] ? mem_buffer[mem_buffer_lru_entry].Data[3][15:8]  : '0;   
+                               bus_o_Data[7:0]   <= bus_i_stgd.Sel[0] ? mem_buffer[mem_buffer_lru_entry].Data[3][7:0]   : '0;   
                                end
                       endcase
-                      bus_o.Tga <= bus_i_stgd.Tga;
-                      bus_o.Tgd <= bus_i_stgd.Tgd;
-                      bus_o.Tgc <= bus_i_stgd.Tgc;
+                      bus_o_Tga <= bus_i_stgd.Tga;
+                      bus_o_Tgd <= bus_i_stgd.Tgd;
+                      bus_o_Tgc <= bus_i_stgd.Tgc;
                   state_idle <= '1;
                   end
       state_flushall: begin             
